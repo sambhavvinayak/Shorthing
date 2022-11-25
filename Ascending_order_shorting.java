19c4d1965306fe7dd685a6e7dc46e00197ad3134
Sam <58564925+sambhavvinayak@users.noreply.github.com>
2022-11-25 10:06:16 -0500

    Update and rename shorting.java to Ascending_order_shorting.java

--- Ascending_order_shorting.java
+++ Ascending_order_shorting.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class shorting {
-
+    
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
         System.out.print("write your array size : ");
